77119014223e7da062ac6a99284a28a940caed7d
cindirowan <cindirowan@users.noreply.github.com>
2016-11-20 15:55:10 -0500

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	console.log("wutsup");
-
-   $('.nav li').hover(function() {
-     $(this).find('.subnav').removeClass('hidden');
-   }, function() {
-     $(this).find('.subnav').addClass('hidden');
-   });
-});
